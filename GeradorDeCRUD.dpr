c0d59b48f670176e2a7dfea0a8b3ce8b9c7f499b
givanildofeitoza <givanildofeitoza@hotmail.com>
2023-09-15 18:00:42 -0300

    frist commit

---
+++ GeradorDeCRUD.dpr
@@ -0,0 +1,14 @@
+program GeradorDeCRUD;
+
+uses
+  Vcl.Forms,
+  GeradorCrud in 'GeradorCrud.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
